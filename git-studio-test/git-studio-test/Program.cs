3413d176392e5b3e34a19aaea10237b4a6453787
Courtney Frey <cfrey@launchcode.org>
2021-07-09 14:04:31 -0500

    first commit

---
+++ git-studio-test/git-studio-test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace git_studio_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Howdy!");
+
+            List<string> name = new List<string>();
+            {
+                name.Add("Willow");
+                name.Add("Alyce");
+            }
+
+            foreach(string n in name)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
